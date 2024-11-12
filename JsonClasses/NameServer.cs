ea5a36ade369e7519544e11dfbcfe06bcbda2610
CrypticBren <crypticbren@gmail.com>
2024-11-12 11:03:15 -0600

    Update NameServer.cs
    changed all the values except the ws one to have port "5000" instead of port "7689" since the default port for asp.net is port "5000" and i kinda forgot to change it to that either way so uh yeah

--- JsonClasses/NameServer.cs
+++ JsonClasses/NameServer.cs
@@ -2,10 +2,10 @@
 {
     public class NameServer
     {
-        public string Auth { get; set; } = "http://localhost:7689";
-        public string API { get; set; } = "http://localhost:7689";
-        public string WWW { get; set; } = "http://localhost:7689";
+        public string Auth { get; set; } = "http://localhost:5000";
+        public string API { get; set; } = "http://localhost:5000";
+        public string WWW { get; set; } = "http://localhost:5000";
         public string Notifications { get; set; } = "ws://localhost:7698";
-        public string Images { get; set; } = "http://localhost:7689";
+        public string Images { get; set; } = "http://localhost:5000";
     }
 }
